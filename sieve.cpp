a06502c11da092f87518e2bfc62b806588604195
calafite <pertinaciouslymedici@gmail.com>
2026-06-19 14:44:08 +0100

    style: use this-> for struct field access

--- sieve.cpp
+++ sieve.cpp
@@ -1,39 +1,38 @@
-
 template<typename T>
-struct Sieve {
+struct Sieve{
     T n;
     vector<bool> is_prime;
     vector<T> primes;
     vector<T> spf;
 
     Sieve(T n) : n(n), is_prime(n + 1, true){
-        is_prime[0] = is_prime[1] = false;
-        for (T p = 2; p * p <= n; p++){
-            if (is_prime[p]){
-                for (T i = p * p; i <= n; i += p) is_prime[i] = false;
+        this->is_prime[0] = this->is_prime[1] = false;
+        for (T p = 2; p * p <= this->n; p++){
+            if (this->is_prime[p]){
+                for (T i = p * p; i <= this->n; i += p) this->is_prime[i] = false;
             }
         }
-        for (T i = 2; i <= n; i++){
-            if (is_prime[i]) primes.pb(i);
+        for (T i = 2; i <= this->n; i++){
+            if (this->is_prime[i]) this->primes.pb(i);
         }
     }
 
     bool query(T x){
-        if (x < 0 || x > n) return false;
-        return is_prime[x];
+        if (x < 0 || x > this->n) return false;
+        return this->is_prime[x];
     }
 
     T count(T x){
-        x = min(x, n);
-        return (T)upper_bound(all(primes), x) - primes.begin();
+        x = min(x, this->n);
+        return (T)upper_bound(all(this->primes), x) - this->primes.begin();
     }
 
     void build_spf(){
-        spf.assign(n + 1, 0);
-        for (T i = 2; i <= n; i++){
-            if (!spf[i]){
-                for (T j = i; j <= n; j += i){
-                    if (!spf[j]) spf[j] = i;
+        this->spf.assign(this->n + 1, 0);
+        for (T i = 2; i <= this->n; i++){
+            if (!this->spf[i]){
+                for (T j = i; j <= this->n; j += i){
+                    if (!this->spf[j]) this->spf[j] = i;
                 }
             }
         }
@@ -42,8 +41,8 @@ struct Sieve {
     map<T, int> factorize(T x){
         map<T, int> factors;
         while (x > 1){
-            factors[spf[x]]++;
-            x /= spf[x];
+            factors[this->spf[x]]++;
+            x /= this->spf[x];
         }
         return factors;
     }
