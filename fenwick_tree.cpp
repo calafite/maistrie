37b874b6bbc55c1a2c394d6b78dfb60444a99070
calafite <pertinaciouslymedici@gmail.com>
2026-06-19 23:31:14 +0100

    refactor: micro-optimisations

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -3,24 +3,18 @@
 using namespace std;
 
 template <typename T> struct FenwickTree {
+  int sz;
   vector<T> bits;
-  int sz, highbit;
 
-  FenwickTree(int n) : sz(n), highbit(1), bits(n + 1, T(0)) {
-    while (highbit < sz)
-      highbit <<= 1;
-  }
+  FenwickTree(int n) : sz(n), bits(n + 1, T(0)) {}
 
-  FenwickTree(const vector<T> &a)
-      : sz((int)a.size()), highbit(1), bits(a.size() + 1, T(0)) {
+  FenwickTree(const vector<T> &a) : sz((int)a.size()), bits(a.size() + 1, T(0)) {
     for (int i = 1; i <= sz; i++) {
       bits[i] += a[i - 1];
       int j = i + (i & -i);
       if (j <= sz)
         bits[j] += bits[i];
     }
-    while (highbit < sz)
-      highbit <<= 1;
   }
 
   T query(int i) const {
@@ -37,11 +31,14 @@ template <typename T> struct FenwickTree {
       bits[i] += add;
   }
 
-  void set(int i, T val) { update(i, val - query(i, i)); }
+  void set(int i, T val) { 
+    update(i, val - query(i, i)); 
+  }
 
   int kth(T k) const {
+    if (sz == 0) return 0;
     int pos = 0;
-    for (int pw = highbit; pw > 0; pw >>= 1) {
+    for (int pw = 1 << __lg(sz); pw > 0; pw >>= 1) {
       if (pos + pw <= sz && bits[pos + pw] < k) {
         pos += pw;
         k -= bits[pos];
